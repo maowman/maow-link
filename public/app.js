8381c2f17f94cd6eb4aeb847fdb784b467b89eca
建中貓貓男 <54308739+MaowMan@users.noreply.github.com>
2020-06-14 16:00:18 +0800

    2020/6/14

---
+++ public/app.js
@@ -0,0 +1,26 @@
+let control_js = new Vue({
+    el: "#control_dom",
+    data: {
+
+    },
+    methods: {
+
+    },
+    mounted: function () {
+    }
+});
+
+let recaptcha_js = new Vue({
+    el: "#recapcha_dom",
+    data: {
+
+    },
+    methods: {
+
+    },
+    mounted: function () {
+
+    }
+})
+
+
